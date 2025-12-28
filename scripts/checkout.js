13c8fe4b6b8a4b1450cb282dbdaaf86bb323778e
PranavRaut062006 <raut.ps2006@gmail.com>
2025-12-28 22:05:54 +0530

    26 Create oop version if cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';  // another syntax we can run all codes in the file 
 
 renderOrderSummary();
 renderPaymentSummary();
