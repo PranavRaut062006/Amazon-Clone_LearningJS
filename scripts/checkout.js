2d5d6e4a67e4467f5084b9e43ef1a9c236062f40
PranavRaut062006 <raut.ps2006@gmail.com>
2025-12-28 23:02:26 +0530

    29 Convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 //import '../data/cart-oop.js';  // another syntax we can run all codes in the file 
-import '../data/cart-class.js';  // another syntax we can run all codes in the file 
+
+//import '../data/cart-class.js';  // another syntax we can run all codes in the file 
 
 renderOrderSummary();
 renderPaymentSummary();
