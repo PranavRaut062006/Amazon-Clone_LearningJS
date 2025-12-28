d4dd8d4c51e09cb4a3f29b99a2458299fb967774
PranavRaut062006 <raut.ps2006@gmail.com>
2025-12-28 22:26:02 +0530

    27 Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
-import '../data/cart-oop.js';  // another syntax we can run all codes in the file 
+//import '../data/cart-oop.js';  // another syntax we can run all codes in the file 
+import '../data/cart-class.js';  // another syntax we can run all codes in the file 
 
 renderOrderSummary();
 renderPaymentSummary();
